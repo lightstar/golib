634ec237f8fbc5b00a750a45e196846b110491ba
LightStar <lightstar@hallea.ru>
2020-11-06 15:34:43 +0300

    Remove fileutil package, use ioutil instead.

--- pkg/config/file.go
+++ pkg/config/file.go
@@ -1,14 +1,15 @@
 package config
 
 import (
+	"io/ioutil"
+
 	"github.com/lightstar/golib/pkg/errors"
-	"github.com/lightstar/golib/pkg/util/fileutil"
 )
 
 // NewFromFile function creates new configuration service using data in some file as a source and chosen encoder.
 // Most likely you will use one of the predefined encoders: JSONEncoder, YAMLEncoder or TOMLEncoder.
 func NewFromFile(name string, encoder Encoder) (*Config, error) {
-	dataBytes, err := fileutil.ReadAll(name)
+	dataBytes, err := ioutil.ReadFile(name)
 	if err != nil {
 		return nil, errors.New("file error").WithCause(err)
 	}
